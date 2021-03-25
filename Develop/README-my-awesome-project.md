94fcd2962ed0c2f9bfea05b59f34560586d6d78c
Corrinne W <corrinneworden@gmail.com>
2021-03-25 13:54:55 -0400

    added license text and badges

---
+++ Develop/README-my-awesome-project.md
@@ -0,0 +1,24 @@
+undefined
+            # my-awesome-project  
+            its so awesome 
+
+            ## Table of Contents  
+            all 
+
+            ## Installation  
+            install 
+
+            ## Usage  
+            use  
+
+            ## Contributing  
+            contribute  
+
+            ## Tests  
+            testing 
+
+            ## Questions  
+            CorrinneW
+            
+            ##License
+            undefined
--- Develop/index.js
+++ Develop/index.js
@@ -6,6 +6,11 @@ const fs = require('fs');
 const questions = [
     {
         type: 'input',
+        message: 'Copyright Owner Name:',
+        name: 'copyrightOwner'
+    },
+    {
+        type: 'input',
         message: 'Project Title: ',
         name: 'projectTitle'
     },
@@ -41,8 +46,14 @@ const questions = [
     },
     {
         type: 'input',
-        message: 'Questions: ',
+        message: 'To direct user questions to your gitHub, provide your gitHub username: ',
         name: 'questions'
+    },
+    {
+        type: 'list',
+        message: 'Select a license:',
+        name: 'license',
+        choices: ['Apache 2.0', 'MIT License', 'GPLv3'],
     }
 ];
 
@@ -51,11 +62,70 @@ function writeToFile(fileName, data) {
     console.log('Welcome to the README generator. Please input your project information (Markdown supported)')
     inquirer
         .prompt(questions)
-        .then (data => {
+        .then(data => {
             //create template literal from data for both the file name and its contents
             const fileName = `README-${data.projectTitle.toLowerCase().split(' ').join('')}.md`;
-            
-            const fileContent = `# ${data.projectTitle}  
+
+            //license text for user-selected license
+            let license;
+            //license badge for user-selected license
+            let licenseBadge;
+            //generates license text and badge
+            const fileLicense = (data) => {
+                const currentYear = new Date().getFullYear();
+
+                const copyrightOwner = data.copyrightOwner;
+
+                if (data.license === 'Apache 2.0') {
+                    licenseBadge = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
+
+                    license = `Copyright ${currentYear} ${copyrightOwner}
+
+                    Licensed under the Apache License, Version 2.0 (the "License");
+                    you may not use this file except in compliance with the License.
+                    You may obtain a copy of the License at
+                 
+                      [Apache.org](http://www.apache.org/licenses/LICENSE-2.0)
+                 
+                    Unless required by applicable law or agreed to in writing, software
+                    distributed under the License is distributed on an "AS IS" BASIS,
+                    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+                    See the License for the specific language governing permissions and
+                    limitations under the License.`
+                } else if (data.license === 'MIT License') {
+                    licenseBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
+
+                    license = `Copyright ${currentYear} ${copyrightOwner}
+
+                    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+                    
+                    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+                    
+                    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`
+                } else if (data.license === 'GPLv3') {
+                    licenseBadge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
+
+                    license = `Copyright (C) ${currentYear}  ${copyrightOwner}
+
+                    This program is free software: you can redistribute it and/or modify
+                    it under the terms of the GNU General Public License as published by
+                    the Free Software Foundation, either version 3 of the License, or
+                    (at your option) any later version.
+                
+                    This program is distributed in the hope that it will be useful,
+                    but WITHOUT ANY WARRANTY; without even the implied warranty of
+                    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+                    GNU General Public License for more details.
+                
+                    You should have received a copy of the GNU General Public License
+                    along with this program.  If not, see [GNU.org](http://www.gnu.org/licenses/).`
+                }
+
+                console.log(license)
+            };
+
+            const fileContent = `${licenseBadge}
+            # ${data.projectTitle}  
             ${data.description} 
 
             ## Table of Contents  
@@ -74,12 +144,15 @@ function writeToFile(fileName, data) {
             ${data.tests} 
 
             ## Questions  
-            ${data.questions}`;
+            ${data.questions}
+            
+            ##License
+            ${license}`;
 
             fs.writeFile(fileName, fileContent, (err) =>
-            err ? console.error(err) : console.log('Success!'))
+                err ? console.error(err) : console.log('Success!'))
         })
-        .catch (error => {
+        .catch(error => {
             if (error.isTtyError) {
                 // Prompt couldn't be rendered in the current environment
                 console.error('prompt could not be rendered in the current environment')
