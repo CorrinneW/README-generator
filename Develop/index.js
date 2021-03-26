ba8e3cc614d13cf81a2774c770504cb99e1fd344
Corrinne W <corrinneworden@gmail.com>
2021-03-26 14:25:05 -0400

    finished getLicense function, added inquirer prompt for user email, corrected const fileName to replace spaces with dash and make all lower case

--- Develop/index.js
+++ Develop/index.js
@@ -6,48 +6,43 @@ const fs = require('fs');
 const questions = [
     {
         type: 'input',
-        message: 'Copyright Owner Name:',
-        name: 'copyrightOwner'
-    },
-    {
-        type: 'input',
-        message: 'Project Title: ',
+        message: 'Please enter your project title: ',
         name: 'projectTitle'
     },
     {
         type: 'input',
-        message: 'Description: ',
+        message: 'Please enter a brief description of your project: ',
         name: 'description'
     },
     {
         type: 'input',
-        message: 'Table of Contents: ',
-        name: 'tableContents'
-    },
-    {
-        type: 'input',
-        message: 'Installation',
+        message: 'Please enter installation instructions:',
         name: 'installation'
     },
     {
         type: 'input',
-        message: 'Usage: ',
+        message: 'Please enter usage instructions: ',
         name: 'usage'
     },
     {
         type: 'input',
-        message: 'Contributing: ',
+        message: 'Please enter instructions for contributing to your project: ',
         name: 'contributing'
     },
     {
         type: 'input',
-        message: 'Tests: ',
+        message: 'Please enter testing procedures: ',
         name: 'tests'
     },
     {
         type: 'input',
-        message: 'To direct user questions to your gitHub, provide your gitHub username: ',
-        name: 'questions'
+        message: 'Please enter your gitHub username: ',
+        name: 'githubUserName'
+    },
+    {
+        type: 'input',
+        message: 'Please enter your email: ',
+        name: 'userEmail'
     },
     {
         type: 'list',
@@ -57,103 +52,117 @@ const questions = [
     }
 ];
 
+//global object to receive data from inquirer
+const answers = {};
+
+//provides badges and license text to  getLicense function and writeToFile function
+const licenses = [
+    {
+        name: 'Apache 2.0',
+        badge: `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`,
+        text: `Licensed under the Apache License, Version 2.0`
+    },
+
+    {
+        name: 'MIT License',
+        badge: `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`,
+        text: `Licensed under the MIT License`
+
+        },
+
+    {
+        name: 'GPLv3',
+        badge: `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`,
+        text: `Licensed under GPLv3`
+    }
+];
+
+//global variables for license badge and text
+let licenseBadge;
+let licenseText;
+
+//function to generate license badge and text from license object
+function getLicense() {
+
+    if (answers.license === 'Apache 2.0') {
+        licenseBadge = licenses[0].badge;
+        licenseText = licenses[0].text;
+    } else if (answers.license === 'MIT License') {
+        licenseBadge = licenses[1].badge;
+        licenseText = licenses[1].text;
+    } else if (answers.license === 'GPLv3') {
+        licenseBadge = licenses[2].badge;
+        licenseText = licenses[2].text;
+    } else {
+        licenseBadge = '';
+        licenseText = '';
+    };
+};
+
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
+//template literal forms basic structure of README
+function writeToFile() {
+    const projectTitle = answers.projectTitle;
+
+    const fileName = projectTitle.replace(/\s+/g, '-').toLowerCase()
+
+    const fileContent = 
+ `${licenseBadge}
+
+ # ${projectTitle}  
+ ${answers.description} 
+
+ ## Table of Contents  
+ * [Installation] (https://github.com/${answers.githubUserName}/${fileName}#installation)
+ * [Usage] (https://github.com/${answers.githubUserName}/${fileName}#usage)
+ * [Contributing] (https://github.com/${answers.githubUserName}/${fileName}#contributing)
+ * [Tests] (https://github.com/${answers.githubUserName}/${fileName}#tests)
+ * [Questions] (https://github.com/${answers.githubUserName}/${fileName}#questions)
+ * [License] (https://github.com/${answers.githubUserName}/${fileName}#license)
+
+ ## Installation  
+ ${answers.installation} 
+
+ ## Usage  
+ ${answers.usage}  
+
+ ## Contributing  
+ ${answers.contributing}  
+
+ ## Tests  
+ ${answers.tests} 
+
+ ## Questions  
+ Please field any additional questions to: GitHub Profile ${answers.githubUserName} (https://github.com/${answers.githubUserName}) Email: ${answers.userEmail}
+ 
+ ## License
+ ${licenseText}`;
+
+    fs.writeFile(`${fileName}.md`, fileContent, (err) => {
+        if (err) {
+            console.error(err)
+        } else {
+            console.log('Success!')
+        }
+    })
+
+
+}
+
+// TODO: Create a function to initialize app
+function init() {
     console.log('Welcome to the README generator. Please input your project information (Markdown supported)')
     inquirer
         .prompt(questions)
         .then(data => {
-            //create template literal from data for both the file name and its contents
-            const fileName = `README-${data.projectTitle.toLowerCase().split(' ').join('')}.md`;
-
-            //gets license badge and text
-            const { licenseBadge, license } = getLicense();
-
-            //generate license badge and text from user selection/input
-            function getLicense(data) {
-                const currentYear = new Date().getFullYear();
-
-                const copyrightOwner = data.copyrightOwner;
-
-                //generates license badge and text
-                if (data.license === 'Apache 2.0') {
-                    licenseBadge = `[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)`;
-
-                    license = `Copyright ${currentYear} ${copyrightOwner}
-
-                    Licensed under the Apache License, Version 2.0 (the "License");
-                    you may not use this file except in compliance with the License.
-                    You may obtain a copy of the License at
-                 
-                      [Apache.org](http://www.apache.org/licenses/LICENSE-2.0)
-                 
-                    Unless required by applicable law or agreed to in writing, software
-                    distributed under the License is distributed on an "AS IS" BASIS,
-                    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-                    See the License for the specific language governing permissions and
-                    limitations under the License.`
-                } else if (data.license === 'MIT License') {
-                    licenseBadge = `[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)`;
-
-                    license = `Copyright ${currentYear} ${copyrightOwner}
-
-                    Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
-                    
-                    The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-                    
-                    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.`
-                } else if (data.license === 'GPLv3') {
-                    licenseBadge = `[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)`;
-
-                    license = `Copyright (C) ${currentYear}  ${copyrightOwner}
-
-                    This program is free software: you can redistribute it and/or modify
-                    it under the terms of the GNU General Public License as published by
-                    the Free Software Foundation, either version 3 of the License, or
-                    (at your option) any later version.
-                
-                    This program is distributed in the hope that it will be useful,
-                    but WITHOUT ANY WARRANTY; without even the implied warranty of
-                    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-                    GNU General Public License for more details.
-                
-                    You should have received a copy of the GNU General Public License
-                    along with this program.  If not, see [GNU.org](http://www.gnu.org/licenses/).`
-                }
-
-                return {
-                    licenseBadge,
-                    license
-                }
-            };
-
-            const fileContent = `${licenseBadge}
-            # ${data.projectTitle}  
-            ${data.description} 
-
-            ## Table of Contents  
-            ${data.tableContents} 
-
-            ## Installation  
-            ${data.installation} 
-
-            ## Usage  
-            ${data.usage}  
-
-            ## Contributing  
-            ${data.contributing}  
-
-            ## Tests  
-            ${data.tests} 
-
-            ## Questions  
-            ${data.questions}
-            
-            ##License
-            ${license}`;
-
-            fs.writeFile(fileName, fileContent, (err) =>
-                err ? console.error(err) : console.log('Success!'))
+            //add data to global object
+            const returnedAnswers = Object.assign(answers, data);
+            //assigns content to global userName at top of document
+            userName = JSON.stringify(returnedAnswers.userName);
+            //generate license badge and text
+            getLicense(returnedAnswers);
+            //generate template literal for readme content
+            writeToFile(returnedAnswers);
         })
         .catch(error => {
             if (error.isTtyError) {
@@ -166,10 +175,6 @@ function writeToFile(fileName, data) {
         });
 }
 
-// TODO: Create a function to initialize app
-function init() {
-    writeToFile();
-}
 
 // Function call to initialize app
 init();
---
+++ Develop/my-project.md
@@ -0,0 +1,30 @@
+[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)
+
+ # My Project  
+ It's a project 
+
+ ## Table of Contents  
+ * [Installation] (https://github.com/CorrinneW/my-project#installation)
+ * [Usage] (https://github.com/CorrinneW/my-project#usage)
+ * [Contributing] (https://github.com/CorrinneW/my-project#contributing)
+ * [Tests] (https://github.com/CorrinneW/my-project#tests)
+ * [Questions] (https://github.com/CorrinneW/my-project#questions)
+ * [License] (https://github.com/CorrinneW/my-project#license)
+
+ ## Installation  
+ install 
+
+ ## Usage  
+ use  
+
+ ## Contributing  
+ contribute  
+
+ ## Tests  
+ test 
+
+ ## Questions  
+ Please field any additional questions to: [CorrinneW] (https://github.com/CorrinneW) Email: corrinneworden@gmail.com
+ 
+ ## License
+ Licensed under the Apache License, Version 2.0
